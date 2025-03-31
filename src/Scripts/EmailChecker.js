cc54b8673201fcb46975e3dc55b3b86654cbb4d8
ashwinn-si <ashwin23cse@gmail.com>
2025-03-31 17:03:06 +0530

    email verification removed

--- src/Scripts/EmailChecker.js
+++ src/Scripts/EmailChecker.js
@@ -1,5 +1,4 @@
 function EmailChecker(email) {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@(gmail\.com|yahoo\.com|outlook\.com|hotmail\.com)$/;
-    return emailRegex.test(email);
+    return true;
 }
 export default EmailChecker;
